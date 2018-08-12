f9de6dc38b5b34aea5c3a1a561a07034181e031f
pzync <shkhrgrv@gmail.com>
2018-08-12 11:13:53 +0530

    applied CSS keyframes animations to Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className = "Hero">
       <div className = "HeroGroup">
-        <h1>1.3 billion transactions and counting..</h1>
+        <h1>1.3 billion transactions <br />and counting..</h1>
         <p>Our products process 1 million+ transactions every single day. Juspay SDKs are present on over 100 million devices and we count Uber, Amazon, Myntra etc among our customers.</p>
         <Link to="/page-2/">Get Started</Link>
       </div>
